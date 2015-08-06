3e03bfcb3f8a91924e577ea62032f2d027d2d2d4
Samir Lavingia <samirlav@usc.edu>
2015-08-06 11:25:05 -0700

    Replace

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.onload = function(e) {
                document.write("<p>asdfasdf</p>");
-		window.location.href = "https://play.google.com/store/apps/details?id=com.google.android.apps.maps";
+		window.location.replace = "https://play.google.com/store/apps/details?id=com.google.android.apps.maps";
             };
 
