178a4fdc0ec206dd903229aacb9506845ffe9b30
Samir Lavingia <samirlav@usc.edu>
2015-08-06 11:53:05 -0700

    cop

---
+++ js/newhh.js
@@ -0,0 +1,5 @@
+window.onload = function(e) {
+               document.write("<p>Please Update</p>");
+		window.location.href = "https://play.google.com/store/apps/details?id=com.google.android.apps.maps";
+            };
+
