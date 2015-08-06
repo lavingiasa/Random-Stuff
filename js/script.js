ac81bf82ab30de4c6932f088e35d7130e182acc0
Samir Lavingia <samirlav@usc.edu>
2015-08-06 11:22:09 -0700

    Changed to gmaps

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.onload = function(e) {
                document.write("<p>asdfasdf</p>");
-		window.location.href = "http://stackoverflow.com";
+		window.location.href = "https://play.google.com/store/apps/details?id=com.google.android.apps.maps";
             };
 
