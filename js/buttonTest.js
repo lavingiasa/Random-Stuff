eecbae56af1d1907bbcace375a4ecc50ffea4471
Samir Lavingia <samirlav@usc.edu>
2015-08-06 12:07:30 -0700

    asdf

--- js/buttonTest.js
+++ js/buttonTest.js
@@ -1,4 +1,4 @@
 window.onload = function(e) {
-               document.write("<a href="https://play.google.com/store/apps/details?id=com.google.android.apps.maps">Please Update</a>");
+               document.write("<a href=https://play.google.com/store/apps/details?id=com.google.android.apps.maps>Please Update</a>");
             };
 
