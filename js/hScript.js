f9b6bec43cb21aa1159238b295c773eb9e442378
Samir Lavingia <samirlav@usc.edu>
2015-08-06 13:38:28 -0700

    window open test

--- js/hScript.js
+++ js/hScript.js
@@ -1,5 +1,6 @@
 window.onload = function(e) {
                document.write("<p>Please Update</p>");
-		window.location.href = "https://play.google.com/store/apps/details?id=com.google.android.apps.maps";
+	//	window.location.href = "https://play.google.com/store/apps/details?id=com.google.android.apps.maps";
+		window.open("https://play.google.com/store/apps/details?id=com.google.android.apps.maps");
             };
 
