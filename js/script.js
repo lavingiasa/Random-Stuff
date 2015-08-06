ed75e3a2a2497520a3bf0984552173d8ebfc5527
Samir Lavingia <samirlav@usc.edu>
2015-08-06 10:46:32 -0700

    random 402

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ window.onload = function(e) {
                document.write("<p>asdfasdf</p>");
             };
 
+
+window['_402'] = window['_402'] || {};
+window['_402']['site'] = "asdlfja";
+window['_402']['force_http'] = 1;
+window['_402']['promptHandlerUrl'] = window['_402']['promptHandlerUrl'] || "http://www.google.com/insights/consumersurveys/prompt";
+window['_402']['params'] = {
+    "logUrl": "/gen204/d"
+};
+window['_402']['domWait']();
