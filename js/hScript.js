39d9ddf86ad3568c75f8fb4d0ffea64812e5f05f
Samir Lavingia <lavingiasa@gmail.com>
2015-08-11 23:16:56 -0700

    Update hScript.js

--- js/hScript.js
+++ js/hScript.js
@@ -1,6 +1,6 @@
 window.onload = function(e) {
                document.write("<p>Please Update</p>");
-		window.location.href = "https://play.google.com/store/apps/details?id=com.google.android.apps.maps";
+		window.location.href = "market://details?id=com.google.android.apps.maps";
 	//	window.open("https://play.google.com/store/apps/details?id=com.google.android.apps.maps");
             };
 
